8f1ad778afb3a5a63cdfead899ff777138740a65
Marvin Xu <marvinxu99@hotmail.com>
2020-07-13 18:19:36 -0700

    change gmail setting to EMAIL_USE_SSL (was EMAIL_USE_TLS)

--- main_project/settings.py
+++ main_project/settings.py
@@ -99,10 +99,10 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = config('MY_GMAIL')
 EMAIL_HOST_PASSWORD = config('MY_GMAIL_PASSWORD')
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-# EMAIL_USE_SSL = True
-# EMAIL_PORT = 465
+EMAIL_USE_SSL = True
+EMAIL_PORT = 465
+# EMAIL_USE_TLS = True
+# EMAIL_PORT = 587
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
